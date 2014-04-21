7c3fd35c8dd21b0bf2d5988ea1d8021b74266a49
Eric <eclazalde@hotmail.com>
2014-04-21 10:34:11 -0600

    Finished most of the character creation data. Just need to add the update combat stats table and send the json for the create method.

--- rpgonline/static/js/character_create.js
+++ rpgonline/static/js/character_create.js
@@ -1,10 +1,13 @@
-
 var user_id = "";
 var name = "";
 var combat_notes = "";
 var ability_list = new Array();
 var weaknesses_list = new Array();
 var attack_list = new Array();
+
+var perks_list = new Array();
+var flaws_list = new Array();
+
 var defense = "";
 var health = "";
 var endurance = "";
@@ -16,11 +19,271 @@ var other_notes = "";
 var portrait_url = "";
 var icon_url = "";
 
-function new_character(user_id, name, combat_notes, ability_list,
-                       weaknesses_list, attack_list, defense,
-                       health, endurance, tv, background, appearance,
-                       personality, other_notes, portrait_url, icon_url) {
+$(document).ready(function() {
+    $.getJSON( '/all_abilities_json', { name: "{{ session['username'] }}" }, function( data ) {
+        $('#abilitiesname_select').find('option').remove();
+        $.each(data, function(index, field) {
+            $('#abilitiesname_select').append($("<option></option>").attr('value', field.id).text(field.name));
+        });
+        //console.log(JSON.stringify(data));
+    });
     
+    $.getJSON( '/all_weaknesses_json', { name: "{{ session['username'] }}" }, function( data ) {
+        $('#weaknessesname_select').find('option').remove();
+        $.each(data, function(index, field) {
+            $('#weaknessesname_select').append($("<option></option>").attr('value', field.id).text(field.name));
+        });
+        //console.log(JSON.stringify(data));
+    });
+    
+    $.getJSON( '/all_perks_json', { name: "{{ session['username'] }}" }, function( data ) {
+        $('#perkname_select').find('option').remove();
+        $.each(data, function(index, field) {
+            $('#perkname_select').append($("<option></option>").attr('value', field.id).text(field.name));
+        });
+        //console.log(JSON.stringify(data));
+    });
+
+    $.getJSON( '/all_flaws_json', { name: "{{ session['username'] }}" }, function( data ) {
+        $('#flawname_select').find('option').remove();
+        $.each(data, function(index, field) {
+            $('#flawname_select').append($("<option></option>").attr('value', field.id).text(field.name));
+        });
+        console.log(JSON.stringify(data));
+    });
+
+    $("#abilities_btn").on("click", add_to_ability_table);
+    $("#weaknesses_btn").on("click", add_to_weakness_table);
+    $("#perkadd_btn").on("click", add_to_perk_table);
+    $("#flawadd_btn").on("click", add_to_flaw_table);
+    $("#attackadd_btn").on("click", add_to_attack_table);
+});
+
+function add_to_ability_table() {
+    var s_ability_id = $('#abilitiesname_select').children(':selected').attr('value');
+    var s_ability_name = $('#abilitiesname_select').children(':selected').text();
+    var s_ability_value = $('#abilitiesvalue_select').children(':selected').text();
+    var s_ability_note = $('#abilitiesnote_text').val();
+    
+    var ability = [s_ability_id, s_ability_name, s_ability_value, s_ability_note];
+    
+    ability_list.push(ability);
+    
+    update_ability_table();
+    
+    //for (var i=0;i<ability.length;i++) {
+    //    console.log(ability[i])
+    //}
+    
+    //console.log(s_ability_id);
+    //console.log(s_ability_name);
+    //console.log(s_ability_value);
+    //console.log(s_ability_note);
 }
 
-function add_
+function update_ability_table() {
+    $("#abilities_table tr").remove();
+    
+    var table_obj = $('#abilities_table tbody');
+    
+    var table_row = $('<tr>', {id: "a_header"});
+    var table_c_name = $('<th>', {html: 'Name'});
+    var table_c_value = $('<th>', {html: 'Value'});
+    var table_c_note = $('<th>', {html: 'Note'});
+    
+    table_row.append(table_c_name);
+    table_row.append(table_c_value);
+    table_row.append(table_c_note);
+    
+    table_obj.append(table_row);
+    
+    for (var i=0;i<ability_list.length;i++) {
+        var table_row = $('<tr>', {id: ability_list[i][0]});
+        var table_c_name = $('<td>', {html: ability_list[i][1]});
+        var table_c_value = $('<td>', {html: ability_list[i][2]});
+        var table_c_note = $('<td>', {html: ability_list[i][3]});
+        
+        table_row.append(table_c_name);
+        table_row.append(table_c_value);
+        table_row.append(table_c_note);
+        
+        table_obj.append(table_row);
+    }
+}
+
+function add_to_weakness_table() {
+    var s_weaknesses_id = $('#weaknessesname_select').children(':selected').attr('value');
+    var s_weaknesses_name = $('#weaknessesname_select').children(':selected').text();
+    var s_weaknesses_value = $('#weaknessesvalue_select').children(':selected').text();
+    var s_weaknesses_note = $('#weaknessesnote_text').val();
+    
+    var weakness = [s_weaknesses_id, s_weaknesses_name, s_weaknesses_value, s_weaknesses_note];
+    
+    weaknesses_list.push(weakness);
+    
+    update_weakness_table();
+}
+
+function update_weakness_table() {
+    $("#weaknesses_table tr").remove();
+    
+    var table_obj = $('#weaknesses_table tbody');
+    
+    var table_row = $('<tr>', {id: "w_header"});
+    var table_c_name = $('<th>', {html: 'Name'});
+    var table_c_value = $('<th>', {html: 'Value'});
+    var table_c_note = $('<th>', {html: 'Note'});
+    
+    table_row.append(table_c_name);
+    table_row.append(table_c_value);
+    table_row.append(table_c_note);
+    
+    table_obj.append(table_row);
+    
+    for (var i=0;i<weaknesses_list.length;i++) {
+        var table_row = $('<tr>', {id: weaknesses_list[i][0]});
+        var table_c_name = $('<td>', {html: weaknesses_list[i][1]});
+        var table_c_value = $('<td>', {html: weaknesses_list[i][2]});
+        var table_c_note = $('<td>', {html: weaknesses_list[i][3]});
+        
+        table_row.append(table_c_name);
+        table_row.append(table_c_value);
+        table_row.append(table_c_note);
+        
+        table_obj.append(table_row);
+    }
+}
+
+function add_to_perk_table() {
+    var s_perk_id = $('#perkname_select').children(':selected').attr('value');
+    var s_perk_name = $('#perkname_select').children(':selected').text();
+    var s_perk_value = $('#perkmultiplier_select').children(':selected').text();
+    var s_perk_note = $('#perknote_text').val();
+    
+    var perk = [s_perk_id, s_perk_name, s_perk_value, s_perk_note];
+    
+    perks_list.push(perk);
+    
+    update_perk_table();
+}
+
+function update_perk_table() {
+    $("#perkselect_table tr").remove();
+    
+    var table_obj = $('#perkselect_table tbody');
+    
+    var table_row = $('<tr>', {id: "w_header"});
+    var table_c_name = $('<th>', {html: 'Perk Name'});
+    var table_c_value = $('<th>', {html: 'Mult'});
+    var table_c_note = $('<th>', {html: 'Note'});
+    
+    table_row.append(table_c_name);
+    table_row.append(table_c_value);
+    table_row.append(table_c_note);
+    
+    table_obj.append(table_row);
+    
+    for (var i=0;i<perks_list.length;i++) {
+        var table_row = $('<tr>', {id: perks_list[i][0]});
+        var table_c_name = $('<td>', {html: perks_list[i][1]});
+        var table_c_value = $('<td>', {html: perks_list[i][2]});
+        var table_c_note = $('<td>', {html: perks_list[i][3]});
+        
+        table_row.append(table_c_name);
+        table_row.append(table_c_value);
+        table_row.append(table_c_note);
+        
+        table_obj.append(table_row);
+    }
+}
+
+function add_to_flaw_table() {
+    var s_flaw_id = $('#flawname_select').children(':selected').attr('value');
+    var s_flaw_name = $('#flawname_select').children(':selected').text();
+    var s_flaw_value = $('#flawmultiplier_select').children(':selected').text();
+    var s_flaw_note = $('#flawnote_text').val();
+    
+    var flaw = [s_flaw_id, s_flaw_name, s_flaw_value, s_flaw_note];
+    
+    flaws_list.push(flaw);
+    
+    update_flaw_table();
+}
+
+function update_flaw_table() {
+    $("#flawselect_table tr").remove();
+    
+    var table_obj = $('#flawselect_table tbody');
+    
+    var table_row = $('<tr>', {id: "w_header"});
+    var table_c_name = $('<th>', {html: 'Flaw Name'});
+    var table_c_value = $('<th>', {html: 'Mult'});
+    var table_c_note = $('<th>', {html: 'Note'});
+    
+    table_row.append(table_c_name);
+    table_row.append(table_c_value);
+    table_row.append(table_c_note);
+    
+    table_obj.append(table_row);
+    
+    for (var i=0;i<flaws_list.length;i++) {
+        var table_row = $('<tr>', {id: flaws_list[i][0]});
+        var table_c_name = $('<td>', {html: flaws_list[i][1]});
+        var table_c_value = $('<td>', {html: flaws_list[i][2]});
+        var table_c_note = $('<td>', {html: flaws_list[i][3]});
+        
+        table_row.append(table_c_name);
+        table_row.append(table_c_value);
+        table_row.append(table_c_note);
+        
+        table_obj.append(table_row);
+    }
+}
+
+function add_to_attack_table() {
+    var s_attack_name = $('#attackname_text').val();
+    var s_attack_roll = $('#attackroll_text').val();
+    var s_attack_dx = $('#attackdx_text').val();
+    var s_attack_cost = $('#attackcost_text').val();
+    var s_attack_note = $('#attacknote_text').val();
+    
+    var attack = [s_attack_name, perks_list, flaws_list, s_attack_roll, s_attack_dx, s_attack_cost, s_attack_note];
+    
+    attack_list.push(attack);
+    
+    update_attack_table();
+}
+
+function update_attack_table() {
+    $("#combatstats_table tr").remove();
+    
+    var table_obj = $('#combatstats_table tbody');
+    
+    var table_row = $('<tr>', {id: "w_header"});
+    var table_c_name = $('<th>', {html: 'Flaw Name'});
+    var table_c_value = $('<th>', {html: 'Mult'});
+    var table_c_note = $('<th>', {html: 'Note'});
+    
+    table_row.append(table_c_name);
+    table_row.append(table_c_value);
+    table_row.append(table_c_note);
+    
+    table_obj.append(table_row);
+    
+    for (var i=0;i<flaws_list.length;i++) {
+        var table_row = $('<tr>', {id: flaws_list[i][0]});
+        var table_c_name = $('<td>', {html: flaws_list[i][1]});
+        var table_c_value = $('<td>', {html: flaws_list[i][2]});
+        var table_c_note = $('<td>', {html: flaws_list[i][3]});
+        
+        table_row.append(table_c_name);
+        table_row.append(table_c_value);
+        table_row.append(table_c_note);
+        
+        table_obj.append(table_row);
+    }
+}
+
+function add__new_character() {
+    
+}
